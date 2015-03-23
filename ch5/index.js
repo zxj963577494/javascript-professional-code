d6533340d7f33a99617bfad1b08414f7f0077cea
zhengxvjiang <zxj123456>
2015-03-23 21:38:19 +0800

    2015年3月23日 21:38:12

---
+++ ch5/index.js
@@ -0,0 +1,26 @@
+
+/*//Object类型声明
+ var person = new Object();
+ person.name = "zxj";
+ person.age = 25;*/
+
+/*
+
+ //对象字面量
+ var person = {
+ name: "zxj",
+ age: 25
+ }*/
+
+/*
+
+//对象访问属性
+alert(person["name"]) //zxj
+alert(person.name) //zxj
+
+var propertyName = "name";
+alert(person[propertyName]); //zxj
+
+person['first name'] = "zxj";
+*/
+
